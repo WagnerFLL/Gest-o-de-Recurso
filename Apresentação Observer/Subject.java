877d740e1c436017d337e4c2e30c43978527d955
Wagner Fontes <wsf@ic.ufal.br>
2017-08-27 20:13:46 -0300

    Delete Subject.java

--- Apresentação Observer/Subject.java
+++
@@ -1,9 +0,0 @@
-package observer.apresentacao;
-
-public interface Subject {
-	
-    public void addObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObservers();
-    
-}
