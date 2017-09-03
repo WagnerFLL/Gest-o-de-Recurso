7bbf979e586685b364fc0c47b19bc242974ff5e1
Wagner Fontes <wsf@ic.ufal.br>
2017-09-02 21:05:46 -0300

    Add files via upload

--- Gestão de recurso [REFATORADO]/src/Activitys/Activity.java
+++ Gestão de recurso [REFATORADO]/src/Activitys/Activity.java
@@ -1,13 +1,13 @@
 package Activitys;
 
 import java.util.LinkedList;
-import Persons.Person;
+import Persons.PersonPrototype;
 
 public abstract class Activity {
 	
 	protected String title;
 	protected String description;
-	protected LinkedList<Person> participants;
+	protected LinkedList<PersonPrototype> participants;
 	protected String material;
 	
 }
